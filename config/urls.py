1ddcd45026db43f8609b1b52399a1b16ab4ab4f1
jess364 <rethu7718@gmail.com>
2025-10-03 02:27:38 +0900

    service

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('homepage.urls'))
+    path('', include('homepage.urls')),
+    path('service/', include('service.urls')),
 ]
 
 # デバッグモードでのみメディアファイルとデバッグツールバーを有効にする
