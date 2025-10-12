3156d6392fee7da2b37b22326fc1088cdd6ed9ff
jess364 <rethu7718@gmail.com>
2025-10-13 07:28:11 +0900

    white

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,9 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = 'django-insecure-+1x+(9#81qvr=bux8=bh705*l0@pm2z4_$nlm)zy1t3o3a3cw!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # 環境変数で制御
+DEBUG = False  # 環境変数で制御
 
-ALLOWED_HOSTS = ['reang.jp', 'www.reang.jp', '163.44.96.248', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['reang.jp', 'www.reang.jp', '163.44.96.248',]
 
 # NOTE: For production deployment, use config/production_settings.py
 # This file contains development settings only
